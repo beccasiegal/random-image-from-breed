3f5c210318ea6735d3ce66aeadc8a630de5dc51a
beccasiegal <46900478+beccasiegal@users.noreply.github.com>
2019-05-13 15:51:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breed/hound/images/random')
+function getDogImage(input) {
+  fetch(`https://dog.ceo/api/breed/${input}/images`)
     //event.preventDefault();
     .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
+    .then(responseJson => {
+     if(responseJson.status==="error"){
+       displayError(responseJson);
+
+     } else {displayResults(responseJson)}
+    }  )
+    $('.results').removeClass('hidden');
+
    
 }
+function displayError(responseJson){
+  $('.images').html(`<p>${responseJson.message}</p>` )
+
+}
 function displayResults(responseJson) {
- //console.log(responseJson);
-$('.images').replaceWith(
-    `<img src="${responseJson.message}">`
+  console.log(responseJson);
+    const randomNumber = Math.floor(Math.random()*responseJson.message.length);
+      $('.images').html(
+    `<img src="${responseJson.message[randomNumber]}">`
   )
-  $('.results').removeClass('hidden');}
+}
 
 function watchForm() {
   $('form').on('submit', event => {
    event.preventDefault();
-    getDogImage();
+   const input = $('#dog-input').val();
+    getDogImage(input);
   });
 }
 
 $(function() {
-  console.log('App loaded! Waiting for submit!');
+//  console.log('App loaded! Waiting for submit!');
   watchForm();
 });
